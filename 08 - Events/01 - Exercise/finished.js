8cb7e41762258d0a9c510e1a54fad09f27658dfe
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2021-04-10 14:19:56 +0700

    Use keyup in the Event exercise

--- 08 - Events/01 - Exercise/finished.js
+++ 08 - Events/01 - Exercise/finished.js
@@ -1,8 +1,8 @@
-function onKeydown(event) {
+function onKeyup(event) {
   const pElem = document.getElementById('result');
   const inputValue = event.target.value;
   pElem.innerHTML = inputValue;
 }
 
 const inputElem = document.getElementById('name');
-inputElem.addEventListener('keydown', onKeydown);
+inputElem.addEventListener('keyup', onKeyup);
