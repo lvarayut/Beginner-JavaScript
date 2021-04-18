1ba111b1768500eb17da239685666a275f5dfd05
Varayut Lerdkanlayanawat <l.varayut@gmail.com>
2021-04-18 15:48:00 +0700

    Fix typo

--- 08 - Events/02 - Exercise/start.js
+++ 08 - Events/02 - Exercise/start.js
@@ -1,4 +1,4 @@
-// 1. Get the square element by using its id
+// 1. Get the square element by using its class
 // 2. The element should Listen to an event called "click"
 // 3. After clicking the element, change the element background color to "indianred"
 
